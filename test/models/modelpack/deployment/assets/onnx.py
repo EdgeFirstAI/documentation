2fbbace11003a000a0376d3c96cb301bbfc51a6d
EdgeFirst AI <support@edgefirst.ai>
2025-05-27 15:18:25 +0000

    Deployed 7b67cde to test with MkDocs 1.6.1 and mike 2.1.3

--- test/models/modelpack/deployment/assets/onnx.py
+++ test/models/modelpack/deployment/assets/onnx.py
@@ -1,3 +1,14 @@
+"""
+This script is an example for deplying ONNX models in a PC. 
+
+Modify the following lines to your specification.
+
+53: model_path = "modelpack.onnx"
+66: image_path = "sample-coffee-cup.jpg"
+
+Run the script using `python onnx_example.py`
+"""
+
 import numpy as np
 from PIL import Image, ImageFont, ImageDraw
 import onnxruntime
